85bf8b41c0a5951dabf15e0b9fd34b4b8fe84aa2
Geoffrey Dijkstra <GeoffreyDijkstra@users.noreply.github.com>
2026-07-16 16:20:31 +0200

    Add timeout
    Package is still used with a different endpoint which might hang, so 10 second timeout.

--- src/MicrosoftTeams.php
+++ src/MicrosoftTeams.php
@@ -80,6 +80,7 @@ class MicrosoftTeams implements LoggerInterface
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
         $result = curl_exec($ch);
         curl_close($ch);
 
